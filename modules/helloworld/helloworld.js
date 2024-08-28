0ddedbcd51d0d5af6cd2696fe3150c5d90b5a5d3
Dalikon <dalik.pan@seznam.cz>
2024-08-28 20:19:47 +0200

    adding electron

--- modules/helloworld/helloworld.js
+++ modules/helloworld/helloworld.js
@@ -1,7 +1,6 @@
 class helloworld extends Module {
     createDom () {
         const wrapper = document.createElement("div");
-        //console.log(this.config.text)
         wrapper.innerHTML = this.config.text;
 
         return wrapper
@@ -10,7 +9,6 @@ class helloworld extends Module {
     async start () {
         //this.sendSocketNotification("AHOJ", "SVETE");
         this.sendNotification("JEE", "EEJ");
-        console.log(this.index);
         if (this.index === 2) {
                 setInterval(() => {
                     this.hide(1000,function(){});
